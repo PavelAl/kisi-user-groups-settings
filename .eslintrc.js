96ae0405d8cbc9f1d6b104d6e74d6d96e147d429
Pavel <pavel.oparin@myget-it.com>
2022-06-01 21:29:27 +0300

    Added storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
   root: true,
   plugins: ['import', 'prettier', 'react'],
-  extends: ['airbnb-typescript/base'],
+  extends: ['airbnb-typescript/base', 'plugin:storybook/recommended'],
   parserOptions: {
-    project: './tsconfig.eslint.json',
+    project: './tsconfig.eslint.json'
   },
   rules: {
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': ['off'],
-    'react/jsx-newline': ['error', { prevent: false }],
+    'react/jsx-newline': ['error', {
+      prevent: false
+    }],
     'import/extensions': ['error', 'never'],
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
-        pathGroups: [
-          {
-            pattern: '~/**',
-            group: 'sibling',
-            position: 'before',
-          },
-        ],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-  },
+    'import/order': ['error', {
+      'newlines-between': 'always',
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+      pathGroups: [{
+        pattern: '~/**',
+        group: 'sibling',
+        position: 'before'
+      }],
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
+      }
+    }]
+  }
 };
